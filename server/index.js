2892bd8d15ad9d0dfad22cc02d17853fd3f95ba4
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2020-05-01 19:03:26 +0530

    created an express server for graphql queries

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const graphqlHTTP = require('express-graphql');
+
+
+const app=express();
+
+app.use('/graphql', graphqlHTTP({
+    
+}))
+
+app.listen(4000,(req,res)=>{
+    console.log("app listens on port 4000")
+})
